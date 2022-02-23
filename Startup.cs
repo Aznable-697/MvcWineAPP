ab0326e98c9f6f0c2b96e3fa1a2c24bbd4a5a17b
Matt W <mattwhiteker@gmail.com>
2022-02-23 14:28:14 -0500

    Added User Login and Sign up
    Added in a user login and sign up tabbed window inside web application, changed the font of the entire application.

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace MvcWineAPP
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,7 @@ namespace MvcWineAPP
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -58,6 +60,7 @@ namespace MvcWineAPP
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
